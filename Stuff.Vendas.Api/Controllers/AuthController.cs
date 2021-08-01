b6bcb82f6d5e175bc5432bc4b5c642e30afe57c1
Bruno Heinzelmann Vieira <brunoheineng@gmail.com>
2021-08-01 02:07:13 -0300

    claims authorization

--- Stuff.Vendas.Api/Controllers/AuthController.cs
+++ Stuff.Vendas.Api/Controllers/AuthController.cs
@@ -7,8 +7,10 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Authorization;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace Stuff.Vendas.Api.Controllers
 {
@@ -42,7 +44,7 @@ namespace Stuff.Vendas.Api.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, isPersistent: true);
-                return Ok(GerarJWT());
+                return Ok(GerarJWT(registerViewModel.Email));
             }
 
             return BadRequest(result.Errors);
@@ -67,16 +69,35 @@ namespace Stuff.Vendas.Api.Controllers
             return Unauthorized("Login e/ou senha inválidos");
         }
 
-        private  string GerarJWT()
+        private async Task<string> GerarJWT(string email)
         {
             try
             {
+                var user = await _userManager.FindByEmailAsync(email);
+                var claims = await _userManager.GetClaimsAsync(user);
+                var roles = await _userManager.GetRolesAsync(user);
+
+                claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+                claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+                claims.Add(new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()));
+                claims.Add(new Claim(JwtRegisteredClaimNames.Nbf,ToUnixEpochDate(DateTime.UtcNow).ToString()));
+                claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(),ClaimValueTypes.Integer64));
+
+                foreach (var userRole in roles)
+                {
+                    claims.Add(new Claim("role", userRole));
+                }
+
+                var identityClaims = new ClaimsIdentity();
+                identityClaims.AddClaims(claims);
+
                 var tokenHandler = new JwtSecurityTokenHandler();
                 var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
                 var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
                 {
                     Issuer = _appSettings.Emissor,
                     Audience = _appSettings.ValidoEm,
+                    Subject = identityClaims,
                     Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
                     SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
                 });
@@ -91,5 +112,7 @@ namespace Stuff.Vendas.Api.Controllers
             }
 
         }
+        private static long ToUnixEpochDate(DateTime date)
+            => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
     }
 }
---
+++ Stuff.Vendas.Api/Controllers/FornecedorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stuff.Vendas.Api.Extensions;
+
+namespace Stuff.Vendas.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[Controller]")]
+    public class FornecedorController : ControllerBase
+    {
+        public FornecedorController()
+        {
+                
+        }
+        [CustomAuthorize("Fornecedor","Editar")]
+        [HttpGet("atualizar")]
+        public IActionResult AlualizarFornecedor()
+        {
+            return Ok("Fornecedor atualizado");
+        }
+
+        [CustomAuthorize("Fornecedor","remover")]
+        [HttpDelete("deletar")]
+        public IActionResult Deletar()
+        {
+            return Ok("Usuário removido");
+        }
+    }
+}
